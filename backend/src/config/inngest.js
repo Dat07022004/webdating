44154c722264f20a2af9b2c271f59b7b8b57d1ab
Dat2004 <128396435+Dat07022004@users.noreply.github.com>
2026-03-09 18:38:55 +0700

    change setup 01

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -2,7 +2,7 @@ import {Inngest} from 'inngest';
 import { connectDB } from './db.js';
 import { User } from '../models/user.model.js';
 
-export const inngest = new Inngest({id:"ecomerce-app"});
+export const inngest = new Inngest({id:"dating-app"});
 
 const syncUser = inngest.createFunction(
     {id:"sync-user"},
